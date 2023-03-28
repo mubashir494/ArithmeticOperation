7c257b5bd454ec0d22e0b3f55f23de8b327f26ad
Muhammad Mubashir <76434740+mubashir494@users.noreply.github.com>
2023-03-28 04:02:39 -0400

    Update ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -16,7 +16,7 @@ public class ArithmeticOperationsTest {
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
 // 		assertEquals(expected, actual);
-		fail('Must fail')
+		fail("Must fail");
 
 	}
     @Test
